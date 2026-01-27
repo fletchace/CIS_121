d6bbd41b4c48d0b4ceeb351359ef8492153e0464
fletchace <fletch.ace.johnson@gmail.com>
2026-01-27 10:10:11 -0600

    practice 3

---
+++ week_3/practice_3.py
@@ -0,0 +1,19 @@
+purchased=int(input("How many units were purchased?: "))
+max_number_of_units = 0
+budget = 400
+
+while purchased <= 10000:
+    if purchased <= 100:
+        price = 3.99
+    else:
+        if purchased <= 300:
+            price = 2.99
+        else:
+            price = 1.99
+    total_cost = price * purchased
+    print(f"The cost of {purchased} is {round(total_cost,2)}")
+    if total_cost < budget and purchased > max_number_of_units:
+        max_number_of_units = purchased
+    purchased = purchased + 1
+
+print(f"The max number of units for $400 is {max_number_of_units}")
