d9f9253ba86a9378899a7d7d46071c41e34d78c3
fletchace <fletch.ace.johnson@gmail.com>
2026-02-11 16:57:47 -0600

    kjljk

--- week_5/lab_5_function/lab_5_function.1.py
+++ week_5/lab_5_function/lab_5_function.1.py
@@ -1,7 +1,9 @@
 def reverse_until(word, stop):
     reverse = ''
     for letter in word:
-        while letter != stop:
+        if (letter != stop):
             reverse = letter + reverse
+        else:
+            break
     print(reverse)
 reverse_until("programming","m")
