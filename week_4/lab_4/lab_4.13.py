916d248cc630f1314d5657b2a7ede402d02bdeb9
fletchace <fletch.ace.johnson@gmail.com>
2026-02-12 15:41:59 -0600

    i give up

--- week_4/lab_4/lab_4.13.py
+++ week_4/lab_4/lab_4.13.py
@@ -1,4 +1,5 @@
 height = int(input("Enter a height: "))
+candle = '*'
 for star in range(1, height + 1):
-    print(star*'*', end = '')
+    print(star*candle, end = '')
     print()
--- week_5/lab_5_mavpass.py
+++ week_5/lab_5_mavpass.py
@@ -6,19 +6,23 @@ def decode_rle(string):
     how_many = ''
     how_many_number = 0
     count = 0
+    flag = 0
     for char in string:
-        if count > 0:
-            how_many_number = int(how_many)
-            final = final + (pattern * how_many)
         if char != '0' and char != '1' and char != '2' and char != '3' and char != '4' \
             and char != '5' and char != '6' and char != '7' and char != '8' and char != '9':
+            pattern += char
+            count = 0
+            flag += 1
+        else:
+            how_many += char
+            count += 1
+        if count == 0 and flag != 1:
+            how_many_number = int(how_many)
+            final = final + (pattern * how_many_number)
             pattern = ''
             how_many = ''
             how_many_number = 0
-            pattern += char
             count += 1
-        else:
-            how_many += char
     print(final)
 
 decode_rle("a3b1c12")
